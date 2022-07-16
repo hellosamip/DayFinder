ea76064a0f8a71faa9c125e2cf517ce519fcc8de
hellosamip <104580564+hellosamip@users.noreply.github.com>
2022-07-16 16:06:29 +0545

    Add files via upload

---
+++ DayFinder.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int getday(int monthnumber, int date) {
+	int codenumbers[] = {4, 0, 3, 0, 3, 6, 2, 4, 5, 0, 1, 3};
+	char days[7][10] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"};
+	monthnumber = monthnumber - 1;
+	int daycode = (codenumbers[monthnumber]+ date) % 7;
+	if (daycode == 0) {
+		printf("2078 /%d /%d is Saturday!!\n", monthnumber, date);
+	}
+	else {	
+		printf("2078 /%d /%d is %s!!\n", monthnumber, date, days[daycode-1]);
+	}
+	return 0;	
+}
+int main(void) {
+	int monthnumber, date;
+	printf("\t\t\t\tWelcome to day finder\n");
+	printf("\t\t\t\t~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Know day of every date of 2078, Enjoy for free!\n");
+	printf("Input the month number: \n");	
+	scanf("%d", &monthnumber);
+	printf("Input date: \n");
+	scanf("%d", &date);
+	
+	if (monthnumber > 0 && monthnumber < 13 && date > 0 && date < 33) {
+		getday(monthnumber, date);
+	}
+	else {
+		printf("Can not proceed..\n");
+	}
+	return 0;
+}
